ef5f100f589563da526665606adb8ece9dd455bf
10000leaves <leaves.h7@gmail.com>
2024-06-03 10:53:22 +0000

    feat: テキストを選択できないように修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import Typing from "@/components/Typing";
 import CircularSlider from "@/components/CircularSlider";
+
 import { reciteState } from "@/atoms/prayerWheelAtom";
 import { useRecoilValue } from "recoil";
 
@@ -11,7 +12,7 @@ export default function Home() {
     <main className="min-h-screen flex flex-col items-center justify-center p-8 space-y-8 bg-gray-900">
 
       {/* アイコンの表示 */}
-      <div className="flex items-center mb-4">
+      <div className="select-none flex items-center mb-4">
         <img src="/icon.svg" alt="icon" className="w-10 h-10 mr-2" />
         <h1 className="text-4xl font-bold text-yellow-400">デジタルマニ車</h1>
       </div>
@@ -20,7 +21,7 @@ export default function Home() {
       <CircularSlider />
 
       {/* カウンターの表示 */}
-      <p className="text-2xl text-gray-200">唱えた回数：<span className="font-semibold text-yellow-400">{recite}</span></p>
+      <p className="select-none text-2xl text-gray-200">唱えた回数：<span className="font-semibold text-yellow-400">{recite}</span></p>
 
       {/* 経文の表示 */}
       <Typing />
